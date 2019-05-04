a134cb1059e5054a4053145501471093a75d7dec
Jannes <zyclotrop.j@gmail.com>
2019-05-04 18:56:46 +1000

    Update config

--- magiclambdafunction/addSecurityHeaders/index.js
+++ magiclambdafunction/addSecurityHeaders/index.js
@@ -4,13 +4,13 @@ const http = require('http');
 const reporturiurl = "https://<YOUR_URL>.report-uri.com"
 
 exports.handler = (event, context, callback) => {
-    
+
     //Get contents of response
     const response = event.Records[0].cf.response;
     const request = event.Records[0].cf.request;
     const headers = response.headers;
     const s3URL = request && request.origin && request.origin.custom && request.origin.custom.domainName;
-    
+
     const getSHAs = () => new Promise((res, rej) => {
         if(!s3URL) {
             console.error("No s3URL found. Details:");
@@ -29,28 +29,28 @@ exports.handler = (event, context, callback) => {
           rej(err);
         });
     });
-    
+
     const fp = [
         "sync-xhr 'none'",
         "document-write 'none'"
     ];
-    
-     headers['strict-transport-security'] = [{key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubdomains; preload'}]; 
-     headers['feature-policy'] = [{key: 'Feature-Policy', value: fp.join("; ")}]; 
-     headers['x-content-type-options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}]; 
-     headers['x-frame-options'] = [{key: 'X-Frame-Options', value: 'DENY'}]; 
-     headers['x-xss-protection'] = [{key: 'X-XSS-Protection', value: '1; mode=block; report=' + reporturiurl + '/r/d/xss/enforce'}]; 
+
+     headers['strict-transport-security'] = [{key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubdomains; preload'}];
+     headers['feature-policy'] = [{key: 'Feature-Policy', value: fp.join("; ")}];
+     headers['x-content-type-options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}];
+     headers['x-frame-options'] = [{key: 'X-Frame-Options', value: 'DENY'}];
+     headers['x-xss-protection'] = [{key: 'X-XSS-Protection', value: '1; mode=block; report=' + reporturiurl + '/r/d/xss/enforce'}];
      headers['referrer-policy'] = [{key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin'}];
-     
+
      headers["report-to"] = [{key: "Report-To", value: JSON.stringify({"group":"default","max_age":31536000,"endpoints":[{"url":"" + reporturiurl + "/a/d/g"}],"include_subdomains":true})}];
      headers["nel"] = [{ key: "NEL", value: JSON.stringify({"report_to":"default","max_age":31536000,"include_subdomains":true}) }];
      headers["expect-ct"] = [{ key: "Expect-CT", value: 'max-age=86400, enforce, report-uri="' + reporturiurl + '/r/d/ct/enforce"' }];
-     
-     
+
+
      getSHAs().then(shas => {
         const scriptSHAs = shas.scriptSHAs;
         const styleSHAs = shas.styleSHAs;
-        //Set new headers 
+        //Set new headers
         // 'strict-dynamic' + sha on external needed see https://www.w3.org/TR/CSP3/#external-hash
         // Chrome not shipped: https://codereview.chromium.org/2784753003/#ps1
         // Firefox not implemented https://bugzilla.mozilla.org/show_bug.cgi?id=1409200
@@ -64,7 +64,7 @@ exports.handler = (event, context, callback) => {
             "frame-src 'self'",
             "object-src 'none'",
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com/css ", // 'strict-dynamic' // 'unsafe-inline' needed for dynamic css // + styleSHAs.join(" ") // can't use because " Note that 'unsafe-inline' is ignored if either a hash or nonce value is present in the source list."
-            "connect-src 'self' https://fonts.googleapis.com",
+            "connect-src 'self' https://fonts.googleapis.com https://zcmsapi.herokuapp.com https://script.google.com",
             "font-src 'self' data: https://fonts.gstatic.com",
             "img-src 'self' data:",
             "media-src 'self'",
