f1aaa7c2b3ca4bc0b49fed0249ce7206810e9a63
Jannes <zyclotrop.j@gmail.com>
2019-07-08 14:20:56 +1000

    .

--- src/src/pages/404.tsx
+++ src/src/pages/404.tsx
@@ -5,8 +5,6 @@ import config from '../../config/SiteConfig';
 import { Link } from 'gatsby';
 import { Anchor } from 'grommet';
 
-import { FlowChart } from "../widget/FlowChart";
-
 const sample = `
 graph LR
     Start --> Stop
@@ -18,7 +16,6 @@ export default class NotFoundPage extends React.Component<any> {
       <Wrapper>
         <h1>NOT FOUND</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-        <FlowChart graph={sample} theme="neutral" />
       </Wrapper>
     );
   }
