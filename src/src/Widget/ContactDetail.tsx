9aac2fcd4197a3689e04d3d6f0b62f17cffdab04
Jannes <zyclotrop.j@gmail.com>
2019-07-14 19:50:38 +1000

    Tmp change

--- src/src/Widget/ContactDetail.tsx
+++ src/src/Widget/ContactDetail.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'gatsby';
 import { Heading } from 'grommet';
+import { Helmet } from "react-helmet";
 import { HeadlineContext } from "../utils/headlineContext";
 
 interface Props {}
@@ -18,7 +19,42 @@ export class ContactDetail extends React.PureComponent<Props> {
 
     return (<HeadlineContext.Consumer>
         {def => (<div>
+          <script type="application/ld+json">{`
+            {
+              "@context": "http://schema.org",
+              "@type": "Organization",
+              "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Paris, France",
+                "postalCode": "F-75002",
+                "streetAddress": "38 avenue de l'Opera"
+              },
+              "email": "secretariat@google.org",
+              "faxNumber": "( 33 1) 42 68 53 01",
+              "member": [
+                {
+                  "@type": "Organization"
+                },
+                {
+                  "@type": "Organization"
+                }
+              ],
+              "alumni": [
+                {
+                  "@type": "Person",
+                  "name": "Jack Dan"
+                },
+                {
+                  "@type": "Person",
+                  "name": "John Smith"
+                }
+              ],
+              "name": "Google.org (GOOG)",
+              "telephone": "( 33 1) 42 68 53 00"
+            }
+          `}</script>
             
+
           </div>)}
     </HeadlineContext.Consumer>);
   }
